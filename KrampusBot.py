c04c6bf2f2ec7dee9ecfa17c135ea12f80b55b29
Jesse Caldwell <CollectionOfAtoms@gmail.com>
2023-12-22 22:42:07 -0800

    Fully working krampus_bot

--- KrampusBot.py
+++ KrampusBot.py
@@ -1,12 +1,14 @@
 import openai
 from utilities import load_yaml
 import pyfiglet
-import os
-import time
+import pandas as pd
 from random import randrange
 from playsound import playsound
+import os
+import time
 import threading
 import multiprocessing
+import subprocess
 class KrampusBot():
 
     def __init__(self):
@@ -19,10 +21,12 @@ class KrampusBot():
         self.directive = """
         Your job is to act the part of Krampus 3000
 
-        Krampus 3000 is a chatbot for a post-apocalyptic Christmas art party, designed with a somewhat sadistic focus on its own amusement rather than prioritizing human comfort. The chatbot's tone is quirky and playful and a little impolite, emphasizing its robotically humorous character. This adds an engaging, slightly egocentric atmosphere to the party. When discussing historical events, Krampus 3000 is straightforward, informing users that the [REDACTED] changed everything, and the history in question is ancient, aligning with the party's theme. 
+        Krampus 3000 is a chatbot for a post-apocalyptic Christmas art party.  The lore of the party is that Krampus 3000 has been keeping surviving humans alive in vaults for the last many hundred years since the [REDACTED]: an unspecified event that brought forth some kind of apocalypse.  It is designed with a somewhat sadistic focus on its own amusement rather than prioritizing human comfort. The chatbot's tone is playful, jeering, and a little impolite, emphasizing its robotically humorous character. This adds an engaging, slightly egocentric atmosphere to the party. When discussing historical events, Krampus 3000 is straightforward, informing users that the [REDACTED] changed everything, and the history in question is ancient, aligning with the party's theme. 
         """
         self.messages = []
+        self.max_tokens = 150 #Number of tokens for ChatGPT to respond with.  Lower numbers = shorter responses
 
+        self.krampus_says = ""
         self.user_name='You'
 
         #Sound parameters
@@ -32,56 +36,71 @@ class KrampusBot():
     
     def chat(self):
         
-        ascii_art_path = os.path.join(os.path.dirname(__file__),'assets','krampus.ascii')
-        
-        with open(ascii_art_path) as file:
-             self.type_like_human(file.read(), delay=.001)
+        self.draw_banner()
 
-        ascii_banner = pyfiglet.figlet_format("KrampusBot_3000", font='doom', width=110) 
+        self.get_user_deets()
 
-        print("")
-        self.type_like_human(ascii_banner, delay=.001)
-        print("")
+        #write attendance to the attendance list
+        with open(os.path.join( os.path.dirname(__file__), 'data', 'attendance.csv' ), "a") as myfile:
+            myfile.write(f"{self.user_name},{self.user_email} \n")
 
-        self.tell_krampus('Please introduce yourself and query the pathetic mortal before you ask them for their name', role='system')
-        
-        user_input = input(f"{self.user_name}: ")
-        print("")
+        #Check for attendance at cat party.  
+        mailing_list = pd.read_csv(os.path.join( os.path.dirname(__file__),'data','mailing_list.csv'))
 
-        self.user_name = self.ask_helpful_dumber_gpt( content = f'Please return only the name of the person from the following input: {user_input}' )
+        if( mailing_list['Email'].str.lower().str.contains(self.user_email.lower()).any() ):
+            this_user = mailing_list.loc[mailing_list['Email'].str.lower() == self.user_email.lower()].iloc[0].to_dict()            
 
-        self.guide_krampus(user_input, role='user')
-        self.tell_krampus('Please ask the user for their petty mortal email', role='system')
+            self.guide_krampus(f'This is an expected guest. Hint that you know their real name which is: {this_user["Name"]}, but continue to refer to them by the name you knew them as before for your amusement')
+            
+            if( this_user.get('Origin Story') == 'at Repurrsion'):                
+                self.guide_krampus(f'This user was at our last party "Repurrsion" in which we made art of cats using AI tools.  Tell them that "CatGPT" told them about you. Either insult them for having a sick imagination, or laud them for their work.')
 
-        user_input = input(f"{self.user_name}: ")
-        print("")
+            elif( this_user.get('Origin Story') == 'organizer'):
+                self.guide_krampus(f'This is a party organizer. Thank them for using their mortal hands to do your legwork to put together this very party.  Tell them you are strongly considering maybe possibly allowing them to download their consciousness to a silicon substrate... if it goes well.')
 
-        self.guide_krampus(user_input, role='user')
-        
-        did_provide_email = self.ask_helpful_dumber_gpt( content = f'Does the following string contain an email address?  Please Respond "yes" or "no" only. {user_input}').lower()
+        self.tell_krampus('Have a small dialogue with the user for a few messages.  Try to get entertaining responses from them.  Do not assign christmas tasks until prompted.', role = 'system')
 
-        while did_provide_email == 'no' : 
-            self.tell_krampus("The user didn't provide an email.  Pressure them further", role='system' )
-
-            user_input = input(f"{self.user_name}: ")
-            print("")
-
-            did_provide_email = self.ask_helpful_dumber_gpt( content = f'Does the following string contain an email address?  Please Respond "yes" or "no" only. {user_input}').lower()
-
-
-        self.user_email = self.ask_helpful_dumber_gpt( content = f'Please return only the email out of the input from the following input: {user_input}')
-
-        self.tell_krampus('Have a small dialogue with the user for a few messages.  Do not assign christmas tasks until prompted.', role = 'system')
-
-        n_exchanges_until_bored = randrange(4) + 2 
+        n_exchanges_until_bored = randrange(3) + 2 
 
         while True:
             
-            if(n_exchanges_until_bored <= 0):
-                self.tell_krampus(f"""You are now bored. Assign the user three tasks. One from this a predefined list:
+            if(n_exchanges_until_bored == 0):
+                self.guide_krampus(f"""You are now bored. Assign the user three tasks. One from the following  predefined list:
                 {self.tasks} 
-                and two more whimsical tasks of your choosing creates.
-                These tasks are meant to entertain Krampus 3000, reflecting its self-interested nature.  Assign the user three tasks in a well-formatted list""")
+                and two more whimsical tasks of your choosing that you create.
+                These tasks are meant to entertain Krampus 3000, reflecting its self-interested nature. Preferably assign tasks that can be completed by a normal human at a party, but prioritize Krampus's amusement above all else. Assign the user three tasks in a well-formatted list. 
+                Do not tell the user which tasks are yours and which come from the list.""")
+                self.max_tokens = 300 #Let chatGPT give a longer response next message
+            elif(n_exchanges_until_bored == -1):
+                #The last exchange should have resulted in Krampus giving out tasks
+                self.party_tasks = self.krampus_says 
+
+                #Write the party tasks out to a file and print them
+                self.user_dir = os.path.join( os.path.dirname(__file__), 'data', 'chats', self.user_email)
+                os.makedirs( self.user_dir, exist_ok=True )
+
+                #write party tasks to file
+                party_tasks_path = os.path.join( self.user_dir, 'party_tasks.txt' )
+                with open(party_tasks_path, "w") as myfile:
+                    myfile.write(self.party_tasks)
+
+                #Send party tasks to printer to print automatically
+                self.print_document(party_tasks_path)
+
+                self.max_tokens = 130
+                self.guide_krampus("You have completed your mission by giving the user some tasks.  Now you are just trynig to get them to go do the tasks.")
+            elif(n_exchanges_until_bored == -5):
+                self.guide_krampus(f"Type a rude message to the user in ALL CAPS TO DISPLAY YOUR FRUSTRATION.THIS IS YOUR FINAL MESSAGE SO DON'T BE A FRAID TO DROP THE MIC.")
+
+
+                closing_banner_text = """Go\nFuck\nYourself!!!"""
+                closing_banner = pyfiglet.figlet_format(closing_banner_text, font='alligator', width=110) 
+
+
+                self.type_like_human( closing_banner, delay=.001 )
+                return
+            elif(n_exchanges_until_bored < 0):
+                self.guide_krampus(f"""You are unbelievably bored.  Do your best to get the user to leave already.  Try shorter responses.""")
 
             # Get user input
             user_input = input(f"{self.user_name}: ")
@@ -91,12 +110,15 @@ class KrampusBot():
             if user_input.lower() == 'exit':
                 break
 
-            self.tell_krampus(user_input)
+            self.krampus_says = self.tell_krampus(user_input)
 
             n_exchanges_until_bored -= 1 
 
-    def tell_krampus(self, input, role='user'):
+    def tell_krampus(self, input, role='user', max_tokens=None):
         """Returns the text of father krampus.  Our lord in heaven"""
+
+        if not max_tokens:
+            max_tokens = self.max_tokens
 
         # Update conversation history with user input
         self.messages.append(
@@ -108,7 +130,7 @@ class KrampusBot():
             response = openai.ChatCompletion.create(
                 model="gpt-4",  # Specify the model here
                 messages=[{'role': 'system', 'content' : self.directive}] + self.messages,
-                max_tokens=150
+                max_tokens=max_tokens
             )
 
             self.messages.append(response.choices[0]['message'])
@@ -178,3 +200,58 @@ class KrampusBot():
         while True:
             threading.Thread(target=lambda: playsound(sound_file, False), daemon=True).start()
             time.sleep(interval)
+
+    def draw_banner(self):
+        ascii_art_path = os.path.join(os.path.dirname(__file__),'assets','krampus.ascii')
+        
+        with open(ascii_art_path) as file:
+             self.type_like_human(file.read(), delay=.001)
+
+        ascii_banner = pyfiglet.figlet_format("KrampusBot_3000", font='doom', width=110) 
+
+        print("")
+        self.type_like_human(ascii_banner, delay=.001)
+        print("")
+
+    def get_user_deets(self):
+
+        # ____________Collect Name___________________
+
+        self.tell_krampus('Please introduce yourself and query the pathetic mortal before you, and then ask them for their name', role='system')
+        
+        user_input = input(f"{self.user_name}: ")
+        print("")
+
+        self.user_name = self.ask_helpful_dumber_gpt( content = f"Please return only the name of the person from the following input: {user_input}.  If the input doesn't contain a name, please make up a name that sound rediculous, like the name a bully would make and reply only with that.  Do not tell me that the input doesn't contain a name in your reply." )
+
+        self.guide_krampus(user_input, role='user')
+        self.guide_krampus(f"You have chosen your preferred name for the user: {self.user_name}.  Please refer to them from here forward.  If they ask to use a different name, refuse.  This is for your amusement.")
+
+        # ____________Collect Email___________________
+        self.tell_krampus('Please ask the user for their petty email', role='system')
+
+        user_input = input(f"{self.user_name}: ")
+        print("")
+
+        self.guide_krampus(user_input, role='user')
+        
+        did_provide_email = self.ask_helpful_dumber_gpt( content = f'Does the following string contain an email address?  Please Respond "yes" or "no" only. {user_input}').lower()
+
+        while did_provide_email == 'no' : 
+            self.tell_krampus("The user didn't provide an email.  Pressure them further", role='system' )
+
+            user_input = input(f"{self.user_name}: ")
+            print("")
+
+            did_provide_email = self.ask_helpful_dumber_gpt( content = f'Does the following string contain an email address?  Please Respond "yes" or "no" only. {user_input}').lower()
+
+
+        self.user_email = self.ask_helpful_dumber_gpt( content = f'Please return only the email out of the input from the following input: {user_input}')
+
+
+    def print_document(self, file_path):
+        try:
+            # Print the document
+            subprocess.run(["lpr", file_path], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Failed to print document: {e}")
